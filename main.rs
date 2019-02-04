edd7967ece921ea12e40d7c5dda6044139a5b090
calidion <calidion@gmail.com>
2019-02-04 15:32:45 +0800

    init account

--- main.rs
+++ main.rs
@@ -1,8 +1,10 @@
 mod types;
+mod account;
 use std::mem;
 
 fn main() {
     assert_eq!(32, std::mem::size_of::<types::hash_t>());
     println!("{}", std::mem::size_of::<types::hash_t>());
+    println!("{}", std::mem::size_of::<account::Account>());
     println!("Hello, world!");
 }
--- types.rs
+++ types.rs
@@ -1,10 +1,17 @@
 
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 pub struct ba32 {
 data:[u8;32]
 }
 pub struct ba64 {
 data:[u8;64]
 }
+
+pub type elliptic_curve_point_t = ba32;
+pub type elliptic_curve_scalar_t = ba32;
+
 pub type hash_t = ba32;
 pub type public_key_t = ba32;
 pub type secret_key_t = ba32;
@@ -12,20 +19,20 @@ pub type key_derivation_t = ba32;
 pub type key_image_t = ba32;
 pub type signature_t = ba64;
 
-struct account_public_address_t
+pub struct account_public_address_t
 {
     spendPublicKey: public_key_t,
     viewPublicKey: public_key_t,
 }
 
-struct account_keys_t
+pub struct account_keys_t
 {
     address: account_public_address_t,
     spendSecretKey: secret_key_t,
     viewSecretKey: secret_key_t,
 }
 
-struct key_pair_t
+pub struct key_pair_t
 {
     publicKey: public_key_t,
     secretKey:secret_key_t,

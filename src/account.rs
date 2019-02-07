def35b3b451c7175ff61a9df4f88ef4e9f8aac3d
calidion <calidion@gmail.com>
2019-02-08 01:47:41 +0800

    init test cases

--- src/account.rs
+++ src/account.rs
@@ -25,27 +25,26 @@ pub struct Account {
 }
 
 pub fn unix_timestamp() -> u64 {
-  return Utc::now().timestamp() as u64;
+    return Utc::now().timestamp() as u64;
 }
 
 impl Address {
-      fn new(spend: PublicKey,  view: PublicKey) -> Address {
-      Address {
-        spend: spend,
-        view: view
-      }
-      }
-
+    fn new(spend: PublicKey, view: PublicKey) -> Address {
+        Address {
+            spend: spend,
+            view: view,
+        }
+    }
 }
 
 impl Keys {
-        fn new(address:Address, spend: SecretKey,  view: SecretKey) -> Keys {
-      Keys {
-        address: address,
-        spend: spend,
-        view: view,
-      }
-      }
+    fn new(address: Address, spend: SecretKey, view: SecretKey) -> Keys {
+        Keys {
+            address: address,
+            spend: spend,
+            view: view,
+        }
+    }
 }
 
 impl Account {
@@ -61,5 +60,27 @@ impl Account {
             timestamp: unix_timestamp(),
             keys: keys,
         }
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    #[test]
+    fn should_get_current_time() {
+        let now1: u64 = unix_timestamp();
+        assert!(now1 > 10000);
+    }
+
+    #[test]
+    fn should_create_account() {
+        let prefix = 0xBB;
+        let acc: Account = Account::new(prefix);
+        let now1: u64 = unix_timestamp();
+
+        assert!(acc.prefix == prefix);
+        assert!(acc.timestamp - now1 < 10);
+        println!("{:x?}", acc.keys.spend);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,11 @@ fn main() {
     println!("Hello, world!");
     println!("{}", account::unix_timestamp());
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
